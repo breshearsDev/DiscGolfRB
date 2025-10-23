b9a9d166648f1f442fc9b2e133b3e245ff376e9a
Rob Breshears <robbreshears@gmail.com>
2025-10-22 21:45:32 -0600

    Start of Disc Golf Project with basic map and buttons in scrollview

--- DiscGolfRB/ContentView.swift
+++ DiscGolfRB/ContentView.swift
@@ -6,16 +6,49 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct ContentView: View {
+    
+    @State var isPresented: Bool = false
+    
+    @State var holes: [Int] = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18]
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            
+            Spacer()
+            Text("Aggie Greens Disc Golf")
+                .font(.largeTitle)
+            Map()
+                
+//            List(holes, id: \.self) { hole in
+//                Text("Hole \(hole)")
+//            }
+            ScrollView(.horizontal) {
+                HStack {
+                    ForEach(1...18, id: \.self) { hole in
+                        Button {
+                            self.isPresented.toggle()
+                        } label: {
+                            Text("Hole \(hole)")
+                                .frame(width: 100, height: 100)
+                                .background(Color.blue)
+                                .foregroundColor(.white)
+                                .cornerRadius(50)
+                        }
+                    }
+                }
+                .padding()
+                .background(.thinMaterial)
+            }
+            
+            .scrollTargetBehavior(.paging)
+            .sheet(isPresented: $isPresented) {
+                Text("Sheet Content")
+            }
         }
-        .padding()
+
     }
 }
 
